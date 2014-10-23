6b532f0f6ca82489f19c2a4b1e1bda37e0e0ad18
Mikela Clemmons <glassresistor@gmail.com>
2014-10-23 08:04:22 -0700

    added basic browserify and mochify stuff

---
+++ {{ cookiecutter.repo_name }}/src/index.js
@@ -0,0 +1,3 @@
+//If this is a library index is where it lives
+// If we were to install this with npm it would be requires like:
+// var {{ cookiecutter.repo_name }} = require('{{ cookiecutter.repo_name }}');
